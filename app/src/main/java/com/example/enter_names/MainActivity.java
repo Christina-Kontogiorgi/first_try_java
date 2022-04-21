fa39b405fff723c4e1f249a6f38040033b119d0b
Christina-Kontogiorgi <103931174+Christina-Kontogiorgi@users.noreply.github.com>
2022-04-21 12:12:38 +0300

    completed

--- app/src/main/java/com/example/enter_names/MainActivity.java
+++ app/src/main/java/com/example/enter_names/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.enter_names;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.enter_names.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binder;
@@ -10,6 +13,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binder = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binder.getRoot());
+
+        //code:
+        binder.botton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String trexonText = "";
+                if (binder.names.getText() != null)
+                    trexonText = binder.names.getText().toString();
+                else
+                    trexonText = "";
+
+                String us = binder.username.getText().toString();
+                String pass = binder.password.getText().toString();
+                String up = us + "," + pass + "\n";
+                trexonText = trexonText + up;
+
+                binder.names.setText(trexonText);
+                /*
+
+                binder.names.setText(up);*/
+
+            }
+
+        });
+        binder.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 binder.username.setText("");
+                 binder.password.setText("");
+                 binder.names.setText("");
+            }
+        });
     }
 }
